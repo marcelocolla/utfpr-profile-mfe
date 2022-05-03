fd1f5953b7ef13529aff1b1189bf702b2ed95c88
Marcelo Colla <marcelo.colla@pravaler.com.br>
2022-05-02 23:02:20 -0300

    fix build module

--- webpack/webpack.prod.config.ts
+++ webpack/webpack.prod.config.ts
@@ -1,12 +1,16 @@
 import { EnvironmentPlugin, Configuration } from 'webpack'
-import ESLintPlugin from 'eslint-webpack-plugin'
 import { CleanWebpackPlugin } from 'clean-webpack-plugin'
 import merge from 'webpack-merge'
+import ESLintPlugin from 'eslint-webpack-plugin'
 
 import commonConfig from './webpack.common'
 
 const prodConfig: Configuration = {
   mode: 'production',
+  output: {
+    publicPath: 'auto',
+    clean: true,
+  },
   plugins: [
     new EnvironmentPlugin(Object.keys(process.env).filter((key) => key.startsWith('REACT_APP_'))),
     new ESLintPlugin({
